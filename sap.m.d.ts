45d0867002baabcb3b512d9c3b30f8e5e21c9929
Andreas Frische <andreasfrische@gmail.com>
2015-10-14 23:35:50 +0200

    sap.m: ShellOptions added ViewSettingsDialog with Options added

--- sap.m.d.ts
+++ sap.m.d.ts
@@ -2,7 +2,7 @@ declare namespace sap.m {
     namespace semantic {
 
     }
-    class NavContainer {
+    class NavContainer extends sap.ui.core.Control {
         // Adds a custom transition to the NavContainer type (not to a particular instance!). 
         addCustomTransition(sName, oTo, oBack);
 
@@ -108,19 +108,262 @@ declare namespace sap.m {
         setWidth(sWidth);
 
         // Navigates to the next page (with drill-down semantic) with the given (or default) animation.
-        to(sPageId : string, sTransitionName:string, oData, oTransitionParameters);
+        to(sPageId: string, sTransitionName?: string, oData?, oTransitionParameters?);
     }
     class App extends NavContainer {
 
     }
-    class Shell {
+    interface SapMShellOptions {
+        title?: string
+        logo?: any //sap.ui.core.URI
+        showLogout?: boolean
+        headerRightText?: string
+        appWidthLimited?: boolean
+        backgroundColor?: any //sap.ui.core.CSSColor
+        backgroundImage?: any //sap.ui.core.URI
+        backgroundRepeat?: boolean
+        backgroundOpacity?: number
+        homeIcon?: any // object
+        app: sap.ui.core.Control
+    }
+    class Shell extends sap.ui.core.Control {
+        constructor(sId?: string, mSettings?: SapMShellOptions);
+        constructor(mSettings: SapMShellOptions);
 
     }
-    class ViewSettingsItem{
+    class ViewSettingsItem extends sap.ui.core.Item {
+        // Getter for property selected. 
+        getSelected();
+
+        // Setter for property selected.
+        setSelected(bSelected);
+    }
+    class ViewSettingsCustomTab extends sap.ui.core.Item {
 
     }
-    class ViewSettingsDialog {
+    interface SapMViewSettingsDialogOptions {
+        title?: string
+        sortDescending?: boolean
+        groupDescending?: boolean
+        sortItems?: sap.m.ViewSettingsItem[]
+        groupItems?: sap.m.ViewSettingsItem[]
+        filterItems?: sap.m.ViewSettingsItem[]
+        presetFilterItems?: sap.m.ViewSettingsItem[]
+        customTabs?: sap.m.ViewSettingsCustomTab[]
+        confirm?: any
+        cancel?: any
+        resetFilters?: any
+    }
+    class ViewSettingsDialog extends sap.ui.core.Control {
+        constructor(sId?: string, mSettings?: SapMViewSettingsDialogOptions);
+        constructor(mSettings?: SapMViewSettingsDialogOptions);
 
+        // Adds some filterItem into the aggregation named filterItems. 
+        addFilterItem(oFilterItem);
+
+        // Adds a group item and sets the association to reflect the selected state
+        addGroupItem(oItem);
+
+        // Adds a preset filter item and sets the association to reflect the selected state
+        addPresetFilterItem(oItem);
+
+        // Adds a sort item and sets the association to reflect the selected state
+        addSortItem(oItem);
+
+        // Attach event handler fnFunction to the 'cancel' event of this sap.m.ViewSettingsDialog.
+        attachCancel(fnFunction, oListener?);
+        attachCancel(oData, fnFunction, oListener?);
+
+        // Attach event handler fnFunction to the 'confirm' event of this sap.m.ViewSettingsDialog.
+        attachConfirm(fnFunction, oListener?);
+        attachConfirm(oData, fnFunction, oListener?);
+
+        // Attach event handler fnFunction to the 'resetFilters' event of this sap.m.ViewSettingsDialog.
+        attachResetFilters(fnFunction, oListener?);
+        attachResetFilters(oData, fnFunction, oListener?);
+
+        // Binder for aggregation customTabs.
+        bindCustomTabs(oBindingInfo);
+
+        // Binder for aggregation filterItems.
+        bindFilterItems(oBindingInfo);
+
+        // Binder for aggregation groupItems.
+        bindGroupItems(oBindingInfo);
+
+        // Binder for aggregation presetFilterItems.
+        bindPresetFilterItems(oBindingInfo);
+
+        // Binder for aggregation sortItems.
+        bindSortItems(oBindingInfo);
+
+        // Destroys all the customTabs in the aggregation named customTabs.
+        destroyCustomTabs();
+
+        // Destroys all the filterItems in the aggregation named filterItems.
+        destroyFilterItems();
+
+        // Destroys all the groupItems in the aggregation named groupItems.
+        destroyGroupItems();
+
+        // Destroys all the presetFilterItems in the aggregation named presetFilterItems.
+        destroyPresetFilterItems();
+
+        // Destroys all the sortItems in the aggregation named sortItems.
+        destroySortItems();
+
+        // Detach event handler fnFunction from the 'cancel' event of this sap.m.ViewSettingsDialog.
+        detachCancel(fnFunction, oListener);
+
+        // Detach event handler fnFunction from the 'confirm' event of this sap.m.ViewSettingsDialog.
+        detachConfirm(fnFunction, oListener);
+
+        // Detach event handler fnFunction from the 'resetFilters' event of this sap.m.ViewSettingsDialog.
+        detachResetFilters(fnFunction, oListener);
+
+        // Fire event cancel to attached listeners.
+        fireCancel(mArguments?);
+
+        // Fire event confirm to attached listeners.
+        fireConfirm(mArguments?);
+
+        // Fire event resetFilters to attached listeners.
+        fireResetFilters(mArguments?);
+
+        // Getter for aggregation customTabs.
+        getCustomTabs();
+
+        // Getter for aggregation filterItems.
+        getFilterItems();
+
+        // Getter for property groupDescending.
+        getGroupDescending();
+
+        // Getter for aggregation groupItems.
+        getGroupItems();
+
+        // Getter for aggregation presetFilterItems.
+        getPresetFilterItems();
+
+        // Returns the selected filters as an array of ViewSettingsItems.
+        getSelectedFilterItems();
+
+        // Get the selected filter object in format {key: boolean}.
+        getSelectedFilterKeys();
+
+        // Get the filter string in the format "filter name (subfilter1 name, subfilter2 name, .
+        getSelectedFilterString();
+
+        // Id of the element which is the current target of the selectedGroupItem association, or null.
+        getSelectedGroupItem();
+
+        // Id of the element which is the current target of the selectedPresetFilterItem association, or null.
+        getSelectedPresetFilterItem();
+
+        // Id of the element which is the current target of the selectedSortItem association, or null.
+        getSelectedSortItem();
+
+        // Getter for property sortDescending.
+        getSortDescending();
+
+        // Getter for aggregation sortItems.
+        getSortItems();
+
+        // Getter for property title.
+        getTitle();
+
+        // Checks for the provided sap.m.ViewSettingsCustomTab in the aggregation named customTabs.
+        indexOfCustomTab(oCustomTab);
+
+        // Checks for the provided sap.m.ViewSettingsItem in the aggregation named filterItems.
+        indexOfFilterItem(oFilterItem);
+
+        // Checks for the provided sap.m.ViewSettingsItem in the aggregation named groupItems.
+        indexOfGroupItem(oGroupItem);
+
+        // Checks for the provided sap.m.ViewSettingsItem in the aggregation named presetFilterItems.
+        indexOfPresetFilterItem(oPresetFilterItem);
+
+        // Checks for the provided sap.m.ViewSettingsItem in the aggregation named sortItems.
+        indexOfSortItem(oSortItem);
+
+        // Inserts a customTab into the aggregation named customTabs.
+        insertCustomTab(oCustomTab, iIndex);
+
+        // Inserts a filterItem into the aggregation named filterItems.
+        insertFilterItem(oFilterItem, iIndex);
+
+        // Inserts a groupItem into the aggregation named groupItems.
+        insertGroupItem(oGroupItem, iIndex);
+
+        // Inserts a presetFilterItem into the aggregation named presetFilterItems.
+        insertPresetFilterItem(oPresetFilterItem, iIndex);
+
+        // Inserts a sortItem into the aggregation named sortItems.
+        insertSortItem(oSortItem, iIndex);
+
+        // Opens the view settings dialog relative to the parent control
+        open(the);
+
+        // Removes all the controls in the aggregation named customTabs.
+        removeAllCustomTabs();
+
+        // Removes all the controls in the aggregation named groupItems.
+        removeAllGroupItems();
+
+        // Removes all the controls in the aggregation named presetFilterItems.
+        removeAllPresetFilterItems();
+
+        // Removes all the controls in the aggregation named sortItems.
+        removeAllSortItems();
+
+        // Removes a customTab from the aggregation named customTabs.
+        removeCustomTab(vCustomTab);
+
+        // Removes a groupItem from the aggregation named groupItems.
+        removeGroupItem(vGroupItem);
+
+        // Removes a presetFilterItem from the aggregation named presetFilterItems.
+        removePresetFilterItem(vPresetFilterItem);
+
+        // Removes a sortItem from the aggregation named sortItems.
+        removeSortItem(vSortItem);
+
+        // Setter for property groupDescending.
+        setGroupDescending(bGroupDescending);
+
+        // Set the selected filter object in format {key: boolean}
+        setSelectedFilterKeys(oSelectedFilterKeys);
+
+        // Set the selected group item (either by key or by item)
+        setSelectedGroupItem(oItem);
+
+        // Set the selected preset filter item
+        setSelectedPresetFilterItem(oItem);
+
+        // Set the selected sort item (either by key or by item)
+        setSelectedSortItem(oItem);
+
+        // Setter for property sortDescending.
+        setSortDescending(bSortDescending);
+
+        // Set the title of the internal dialog
+        setTitle(sTitle);
+
+        // Unbinder for aggregation customTabs.
+        unbindCustomTabs();
+
+        // Unbinder for aggregation filterItems.
+        unbindFilterItems();
+
+        // Unbinder for aggregation groupItems.
+        unbindGroupItems();
+
+        // Unbinder for aggregation presetFilterItems.
+        unbindPresetFilterItems();
+
+        // Unbinder for aggregation sortItems.
+        unbindSortItems();
     }
     namespace MessageToast {
         function show(sMessage: string, mOptions?): void;
